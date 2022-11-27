5cf87be1a471545e11734ed61414266548e88d0d
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-11-27 15:56:09 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(skate="roller-skate") {
+ 
+   return(`This Saturday, I want to ${skate}!`);
+    
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+const mondayWork = (function(office="go to the office") {
+    return  (`This Monday, I will ${office}.`)
+})
+fn()
+fn("work from home")
+
+function wrapAdjective (flair="*") {
+    return function(imso="special") {
+        return `You are ${flair}${imso}${flair}!`
+    }
+};
+const encouragingPromptFunction = wrapAdjective("%")("a dedicated programmer")
